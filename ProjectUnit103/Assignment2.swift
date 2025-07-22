fb6b7506a08bc0fe77a5688f13ded36811068160
DS4506 <davidsmith4506@gmail.com>
2025-07-21 22:35:40 -0500

    Assignment 2

---
+++ ProjectUnit103/Assignment2.swift
@@ -0,0 +1,20 @@
+//
+//  Assignment2.swift
+//  ProjectUnit103
+//
+//  Created by Willie Earl on 7/21/25.
+//
+
+import SwiftUI
+
+struct Assignment2: View {
+    var body: some View {
+        
+        Text("Assignment2")
+        
+    }
+}
+
+#Preview {
+    Assignment2()
+}
